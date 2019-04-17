bf00794b47c2b0648fc560abdbf49e028f40bdf9
JCGrant <jamescolin.grant@gmail.com>
2019-04-17 20:02:49 +0100

    Implement randLetter and randString3

--- src/Set1RandomNumbers.hs
+++ src/Set1RandomNumbers.hs
@@ -13,3 +13,12 @@ genMany :: (Seed -> (a, Seed)) -> Seed -> [a]
 genMany rand s = x : genMany rand s'
   where
     (x, s') = rand s
+
+-- 2. Random character generation
+randLetter :: Seed -> (Char, Seed)
+randLetter s = (toLetter x, s')
+  where
+    (x, s') = rand s
+
+randString3 :: String
+randString3 = take 3 $ genMany randLetter (mkSeed 1)

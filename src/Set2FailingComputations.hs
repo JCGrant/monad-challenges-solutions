83ed11d894cdd8d0b9c29ccfa0f206d0a541fc9e
JCGrant <jamescolin.grant@gmail.com>
2019-04-18 00:02:31 +0100

    Implement Maybe library

--- src/Set2FailingComputations.hs
+++ src/Set2FailingComputations.hs
@@ -18,3 +18,33 @@ instance Eq a => Eq (Maybe a) where
   Nothing == Nothing = True
   Just x == Just y = x == y
   _ == _ = False
+
+instance Ord a => Ord (Maybe a) where
+  Just x <= Just y = x <= y
+
+-- 2. Build a library of things that can fail
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:xs) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (x:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay key ((k, v):kvs)
+  | key == k = Just v
+  | otherwise = lookupMay key kvs
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay x 0 = Nothing
+divMay x y = Just $ x / y
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay xs = Just $ foldl1 max xs
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay xs = Just $ foldl1 min xs

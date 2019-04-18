89a6d5fb838b3638e19e4d9eaf99fcd818837ee5
JCGrant <jamescolin.grant@gmail.com>
2019-04-18 16:16:45 +0100

    Implement Card and allCards

--- src/Set3Combinations.hs
+++ src/Set3Combinations.hs
@@ -12,3 +12,18 @@ allPairs xs ys = allPairs' xs ys
     allPairs' [] _ = []
     allPairs' (x:xs) [] = allPairs' xs ys
     allPairs' allX@(x:_) (y:ys) = (x, y) : allPairs' allX ys
+
+-- 2. Poker hands
+data Card =
+  Card Int
+       String
+
+instance Show Card where
+  show (Card r s) = show r ++ s
+
+allCards :: [Int] -> [String] -> [Card]
+allCards xs ys = allCards' xs ys
+  where
+    allCards' [] _ = []
+    allCards' (x:xs) [] = allCards' xs ys
+    allCards' allX@(x:_) (y:ys) = Card x y : allCards' allX ys

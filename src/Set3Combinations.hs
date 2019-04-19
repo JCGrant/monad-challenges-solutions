c9c241ca0b3ff3369f02a0bf1b444f1934353631
JCGrant <jamescolin.grant@gmail.com>
2019-04-19 15:27:43 +0100

    Implement allCombs3

--- src/Set3Combinations.hs
+++ src/Set3Combinations.hs
@@ -27,3 +27,12 @@ allCombs f xs ys = allCombs' xs ys
     allCombs' [] _ = []
     allCombs' (x:xs) [] = allCombs' xs ys
     allCombs' allX@(x:_) (y:ys) = f x y : allCombs' allX ys
+
+-- 4. Combinations of three things
+allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3 f xs ys zs = allCombs3' xs ys zs
+  where
+    allCombs3' [] _ _ = []
+    allCombs3' (x:xs) [] _ = allCombs3' xs ys zs
+    allCombs3' xs (y:ys) [] = allCombs3' xs ys zs
+    allCombs3' allX@(x:_) allY@(y:ys) (z:zs) = f x y z : allCombs3' allX allY zs

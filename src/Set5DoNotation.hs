7e3bd44eb6c4f361ac86e374860385483390f996
JCGrant <jamescolin.grant@gmail.com>
2019-04-19 23:47:07 +0100

    Implement Set 1 with do notation

--- src/Set5DoNotation.hs
+++ src/Set5DoNotation.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE MonadComprehensions #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TupleSections #-}
 
 module Set5DoNotation where
 
 import MCPrelude
+import Set1RandomNumbersWithMonads (randInt, randLetter)
+import Set4CommonAbstractions hiding (Monad, return)
 
 -- 2. Do Notation – operators
 class Monad m where
@@ -11,3 +14,27 @@ class Monad m where
   return :: a -> m a
   fail :: String -> m a
   fail = undefined
+
+-- 3. Do Notation – Set 1
+instance Monad Gen where
+  (>>=) g f =
+    Gen $ \s ->
+      case runGen g s of
+        (x, s') -> runGen (f x) s'
+  return x = Gen (x, )
+
+makeRandom = randInt
+
+fiveRands :: Gen [Integer]
+fiveRands = do
+  sequence $ replicate 5 makeRandom
+
+randString3 :: Gen String
+randString3 = do
+  sequence $ replicate 5 randLetter
+
+generalPair :: Gen a -> Gen b -> Gen (a, b)
+generalPair g1 g2 = do
+  x <- g1
+  y <- g2
+  return (x, y)
